316a4806004f2139f0226c491ae33c07875b8f17
MARCILIOREIS\email <emaildomarcilio@gmail.com>
2018-06-05 22:12:32 -0300

    Fluxo de Respostas

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,11 +11,50 @@ import { FRASES } from './frase-mock';
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
-  public instrucao: string = "Traduza a frase:";
+  public instrucao = 'Traduza a frase:';
+  public resposta: string;
 
-  constructor() { console.log(this.frases) }
+  public rodada = 0;
+  public rodadaFrase: Frase;
+
+  public progresso = 0;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+  }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    // console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
+
+      alert('A tradução está correta');
+
+      // Troca pergrunta da rodada
+      this.rodada++;
+
+      // progresso
+      this.progresso = this.progresso + (100 / this.frases.length);
+      console.log(this.progresso);
+
+      // Atualiza o objeto rodadaFrase
+      this.rodadaFrase = this.frases[this.rodada];
+
+    } else {
+      alert('A tradução não está correta');
+    }
+
+
+
+
   }
 
 }
